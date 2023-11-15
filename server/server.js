77b24fbf46a4a0bbee7d041767370bca45cad9d4
Tasnia Nowrin <nowrint@seas.upenn.edu>
2023-11-15 13:46:01 -0700

    routes & route handlers

--- server/server.js
+++ server/server.js
@@ -1,23 +1,31 @@
-const express = require('express');
-const cors = require('cors');
-const config = require('./config');
-const routes = require('./routes');
+const express = require("express");
+const cors = require("cors");
+const config = require("./config");
+const routes = require("./routes");
 
 const app = express();
-app.use(cors({
-  origin: '*',
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // We use express to define our various API endpoints and
 // provide their handlers that we implemented in routes.js
-app.get('/author/:type', routes.author);
-app.get('/editorial', routes.editorial);
-app.get('/city/:city_id', routes.city);
-app.get('/state/:state_id', routes.state);
-app.get('/search_cities', routes.search_cities);
+// app.get("/author/:type", routes.author);
+app.get("/top_cities", routes.top_cities);
+app.get("/top_states", routes.top_states);
+app.get("/expensive_cities", routes.expensive_cities);
+app.get("/safest_cities", routes.safest_cities);
+app.get("/city", routes.city);
+app.get("/top_housingmarket", routes.top_housingmarket);
+app.get("/monthly_house_prices", routes.monthly_house_prices);
+app.get("/search_cities", routes.search_cities);
 
 app.listen(config.server_port, () => {
-  console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
+  console.log(
+    `Server running at http://${config.server_host}:${config.server_port}/`
+  );
 });
 
 module.exports = app;
