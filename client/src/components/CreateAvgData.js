1143eebe77de636fd123f9e8272857d6be83c694
Jinyeong <jinyeongpark1205@gamil.com>
2023-11-26 14:14:51 -0800

    fix Avg rent price with useEffect

--- client/src/components/CreateAvgData.js
+++ client/src/components/CreateAvgData.js
@@ -1,28 +1,59 @@
 import React from 'react'
 
-const CreateAvgData = (resJson) => {
-    const groupedData = resJson.reduce((acc, entry) => {
-        const yearMonthKey = `${entry.year}-${entry.month}`;
-        if (!acc[yearMonthKey]) {
-          acc[yearMonthKey] = { sum: 0, count: 0 };
-        }
-        acc[yearMonthKey].sum += entry.median_sale_price;
-        acc[yearMonthKey].count += 1;
-        console.log('acc', acc)
-        return acc;
+const CreateAvgData = (resJson, category) => {
+    try {
+       
+        const groupedData = resJson.reduce((acc, entry) => {
+            const yearMonthKey = `${entry.year}-${entry.month}`;
+            if (!acc[yearMonthKey]) {
+              acc[yearMonthKey] = { sum: 0, count: 0 };
+            }
+    
+            if (category === 'Avg Home Price')  {
+                acc[yearMonthKey].sum += entry.median_sale_price;
+                acc[yearMonthKey].count += 1;
+            } else if (category === 'Avg Rent Price') {
+                acc[yearMonthKey].sum += entry.rental_price;
+                acc[yearMonthKey].count += 1;
+            }
+            // console.log('acc', acc)
+            return acc;
+    
+          }, {});
+         
+          const averagedData = Object.keys(groupedData).map((yearMonthKey) => {
+            const [year, month] = yearMonthKey.split('-');
+    
+    
+            if (category === 'Avg Home Price')  {
+                return {
+                    year: parseInt(year),
+                    month: parseInt(month),
+                    avg_median_sale_price: groupedData[yearMonthKey].sum / groupedData[yearMonthKey].count,
+                  };
+            } else if (category === 'Avg Rent Price') {
+                return {
+                    year: parseInt(year),
+                    month: parseInt(month),
+                    avg_rental_price: groupedData[yearMonthKey].sum / groupedData[yearMonthKey].count,
+                  };
+            }
+          });
+          
+       
 
-      }, {});
-     
-      const averagedData = Object.keys(groupedData).map((yearMonthKey) => {
-        const [year, month] = yearMonthKey.split('-');
-        return {
-          year: parseInt(year),
-          month: parseInt(month),
-          avg_median_sale_price: groupedData[yearMonthKey].sum / groupedData[yearMonthKey].count,
-        };
-      });
-      console.log('averagedData', averagedData)
-      return averagedData;
+        // Check if averagedData is undefined or has unexpected value
+        if (!averagedData) {
+            console.error('CreateAvgData returned undefined or unexpected value.');
+            return [];
+        }
+
+        return averagedData;
+    } catch (error) {
+        console.error('Error creating average data:', error);
+        return [];
+    }
+    
 }
 
 export default CreateAvgData
--- client/src/components/LineChart.js
+++ client/src/components/LineChart.js
@@ -12,47 +12,66 @@ const config = require('../config.json');
 
 const LineChart = ({ city, state }) => {
   const [category, setCategory] = React.useState('Avg Home Price');
+  // const [initialData, SetInitialData] = useState([]);
   const [averagedData, setAveragedData] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
 
-   // 1. when a page loading, first default setting is Avg Home Price option.- only run once
+   // 1. when a page loading for the first time, run only once.
    useEffect(() => {
     const fetchDataAndSetAveragedData = async () => {
       try {
         let endpoint = `http://${config.server_host}:${config.server_port}/monthly_house_prices?city=${city}&state=${state}`;
-  
+        
         if (endpoint) {
           console.log("endpoint", endpoint);
           const response = await fetch(endpoint);
           const resJson = await response.json();
-          console.log("resJson", resJson);
-  
+
           if (resJson.length > 0) {
-            const reorganizedData = CreateAvgData(resJson);
-            console.log("reorganizedData", reorganizedData);
-            setAveragedData(reorganizedData);
+            // call CreateAvgData fn
+            const reorganizedData = CreateAvgData(resJson, category);
+            console.log('reorganizedData0', reorganizedData);
+            
+
+            const processedData = 
+            {
+              labels: reorganizedData.map((d)=> d.year),
+              datasets: [{
+                  label: category,
+                  data: reorganizedData.map((d)=> d.avg_median_sale_price)
+              }]
+            }
+            setAveragedData(processedData);
+
+            // console.log('averagedData', averagedData);
           } else {
             console.log('No data available');
           }
         }
       } catch (error) {
         console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false); // Set loading to false when the data fetching is complete
       }
     };
   
     fetchDataAndSetAveragedData();
   }, []);
 
+    //2. Update when category changes
+    useEffect(() => {
+      fetchData(city, state, category)
+  
+    }, [category]);
+
   // Make fetchData an async function
   const fetchData = async (city, state, category) => {
-    console.log('city, state, category', city, state, category)
     setLoading(true);
     let endpoint;
-  
+
     switch (category) {
       case 'Avg Home Price':
-        console.log("case -avg home price")
         endpoint = `http://${config.server_host}:${config.server_port}/monthly_house_prices?city=${city}&state=${state}`;
         break;
       case 'Avg Rent Price':
@@ -61,90 +80,69 @@ const LineChart = ({ city, state }) => {
       default:
         endpoint = '';
     }
-  
+    console.log('endpoint', endpoint)
     try {
       if (endpoint) {
-        console.log("endpoint", endpoint)
         const response = await fetch(endpoint);
         const resJson = await response.json();
-        console.log("resJson", resJson)
+        console.log('resJson.length', resJson.length)
         if (resJson.length > 0) {
-          setAveragedData(CreateAvgData(resJson))
-         
-          // move this to CreateAvgData.js file
-          // const groupedData = resJson.reduce((acc, entry) => {
-          //   const yearMonthKey = `${entry.year}-${entry.month}`;
-          //   if (!acc[yearMonthKey]) {
-          //     acc[yearMonthKey] = { sum: 0, count: 0 };
-          //   }
-          //   acc[yearMonthKey].sum += entry.median_sale_price;
-          //   acc[yearMonthKey].count += 1;
-          //   console.log('acc', acc)
-          //   return acc;
+          const reorganizedData = CreateAvgData(resJson, category);
+          console.log('reorganizedData1', reorganizedData)
 
-          // }, {});
-         
-          // const averagedData = Object.keys(groupedData).map((yearMonthKey) => {
-          //   const [year, month] = yearMonthKey.split('-');
-          //   return {
-          //     year: parseInt(year),
-          //     month: parseInt(month),
-          //     avg_median_sale_price: groupedData[yearMonthKey].sum / groupedData[yearMonthKey].count,
-          //   };
-          // });
-          // console.log('averagedData', averagedData)
-          // return averagedData;
-         // --------------------------------------
-        //   setAveragedData((prevChartData) => {
-        //     return {
-        //       ...prevChartData,
-        //       labels: averagedData.map((d) => `${d.year}-${d.month}`),
-        //       datasets: [
-        //         {
-        //           label: category, // Update the label based on the selected category
-        //           data: averagedData.map((d) => d.avg_median_sale_price),
-        //         },
-        //       ],
-        //     };
-        //   });
-        // } else {
-        //   console.log('No data available');
-        //   return [];
-        // }
+          if (reorganizedData) {
+            console.log('reorganizedData2', reorganizedData)
+
+            if (category === 'Avg Home Price')  {
+              setAveragedData({
+                labels: reorganizedData.map((d) => d.year),
+                datasets: [
+                  {
+                    label: category,
+                    data: reorganizedData.map((d) => d.avg_median_sale_price),
+                  },
+                ],
+              });
+            } else if (category === 'Avg Rent Price') {
+              console.log('category is Avg Rent Price')
+                setAveragedData({
+                  labels: reorganizedData.map((d) => d.year),
+                  datasets: [
+                    {
+                      label: category,
+                      data:  reorganizedData.map((d) => d.avg_rental_price),
+                    },
+                  ],
+                });
+
+            }
+          } else {
+            console.log('CreateAvgData returned undefined or unexpected value.');
+            setAveragedData([]); // Set an empty array if there is an issue with the data
+          }
+        } else {
+          console.log('No data available');
+          setAveragedData([]); // Set an empty array if there is no data
+        }
       }
-    }
     } catch (error) {
       console.error('Error fetching data:', error);
     } finally {
       setLoading(false);
     }
   };
- 
+
   
 
-  //2. when user select other option other than Avg Home Price, useEffect triggers.
-  // useEffect(() => {
-  //   // Update chartData when selectedData changes
-  //   setAveragedData((prevChartData) => {
-  //     return {
-  //       ...prevChartData,
-  //       labels: averagedData.map((d) => `${d.year}-${d.month}`),
-  //       datasets: [
-  //         {
-  //           label: category, // Update the label based on the selected category
-  //           data: averagedData.map((d) => d.avg_median_sale_price),
-  //         },
-  //       ],
-  //     };
-  //   });
-  // }, [averagedData, category]);
+
 
   const handleChange = (event) => {
     const newCategory = event.target.value;
-  
     setCategory(newCategory);
-  
-    fetchData(city, state, newCategory)
+    setLoading(true);
+    
+    // fetchData(city, state, newCategory)
+
    
   };
 
@@ -164,115 +162,13 @@ const LineChart = ({ city, state }) => {
           </Select>
         </FormControl>
       </Box>
-      {/* {loading ? (
+      {loading ? (
         <p>Loading...</p>
       ) : (
-        <Line data={chartData} />
-      )} */}
-      <Line data={{
-        labels: averagedData.map((d)=> d.year),
-        datasets: [{
-            label: "Avg Home Price",
-            data: averagedData.map((d)=> d.avg_median_sale_price)
-        }]
-        }} />
+        <Line data={averagedData && averagedData.labels ? averagedData : {}} />
+      )}
     </>
   );
 };
 
 export default LineChart;
-
-
-
-// const LineChart = ({city, state}) => {
-//   const [category, setCategory] = React.useState('Avg Home Price');
-//   const [selectedData, setSelectedData] = useState([]);
-
-
-//   const fetchData = (city, state, category) => {
-//     setLoading(true);
-//     // Update the API endpoint based on the selected option, page size, and page number
-//     let endpoint;
-//     switch (option) {
-//       case 'Avg Home Price':
-//         endpoint = `http://${config.server_host}:${config.server_port}/monthly_house_prices?city=${city}&state=${state}`;
-//         break;
-//       case 'Avg Rent Price':
-//         endpoint = `http://${config.server_host}:${config.server_port}/monthly_rent_prices?city=${city}&state=${state}`;
-//         break;
-//       default:
-//         endpoint = '';
-//     }
-
-//     if (endpoint) {
-//       fetch(endpoint)
-//         .then((res) => res.json())
-//         .then((resJson) => {
-
-//         if (resJson.length > 0){
-//           setSelectedData((prevData) => [...prevData, ...resJson]);
-//         }
-//         else {
-//             setHasMorePages(false);
-
-//         }
-//         })
-//         .catch((error) => {
-//           console.error('Error fetching data:', error);
-//         });
-//     }
-//   };
-
-//   useEffect(() => {
-//     // Update selectedData based on the selected category
-//     if (category === 'Avg Home Price') {
-//       // Assuming chartData.datasets[0] corresponds to Avg Home Price data
-//       setSelectedData(chartData.datasets[0].data);
-//     } else if (category === 'Avg Rent Price') {
-//       // Assuming chartData.datasets[1] corresponds to Avg Rent Price data
-//       setSelectedData(chartData.datasets[1].data);
-//     } else if (category === 'Total Crime') {
-//       // Assuming chartData.datasets[2] corresponds to Total Crime data
-//       setSelectedData(chartData.datasets[2].data);
-//     }
-//   }, [category, chartData]);
-
-//   const handleChange = (event) => {
-//     setCategory(event.target.value);
-//     setData
-//   };
-
-//   // const updatedChartData = {
-//   //   ...chartData,
-//   //   datasets: [
-//   //     {
-//   //       label: category,
-//   //       data: selectedData,
-//   //       // You may need to adjust other properties based on your actual chart configuration
-//   //     },
-//   //   ],
-//   // }
-//   return (
-//     <>
-//       <Box display="flex" justifyContent="space-between">
-//         <FormControl sx={{ m: 1, minWidth: 120, marginLeft: 'auto' }}>
-//           <Select
-//             value={category}
-//             onChange={handleChange}
-//             displayEmpty
-//             inputProps={{ 'aria-label': 'Without label' }}
-//           >
-//             <MenuItem value="Avg Home Price">Avg Home Price</MenuItem>
-//             <MenuItem value="Avg Rent Price">Avg Rent Price</MenuItem>
-//             <MenuItem value="Total Crime">Total Crime</MenuItem>
-//           </Select>
-//         </FormControl>
-//       </Box>
-//       <Line data={selectedData}/>
-//     </>
-   
-//   )
-// }
-
-// export default LineChart
-
