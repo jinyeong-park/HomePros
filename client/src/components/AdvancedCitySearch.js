0d8533fd45822b28ac4f2fa97269392c318d8fc7
Aditya Garg <gargkaditya@gmail.com>
2023-12-04 15:39:11 -0800

    change advanced search styling/layout

--- client/src/components/AdvancedCitySearch.js
+++ client/src/components/AdvancedCitySearch.js
@@ -49,34 +49,38 @@ const AdvancedSearchModal = ({ open, onClose, onApplyFilters }) => {
           top: '50%',
           left: '50%',
           transform: 'translate(-50%, -50%)',
-          width: 600,
+          width: 1200,
+          height: 550,
           bgcolor: 'background.paper',
           boxShadow: 24,
           p: 3,
         }}
       >
-        <Typography variant="h6">Advanced Search Filters</Typography>
-
-        <Grid container rowSpacing={ 1} columnSpacing={10} >
-        <Grid item xs={6}>
+        <Typography variant="h5">Advanced Search Filters</Typography>
+        <br></br>
+        <Grid container rowSpacing={1} columnSpacing={10} justifyContent="space-around" >
+        <Grid item xs={5}>
         <Typography gutterBottom>Home Price Range</Typography>
+        <br></br>
+        <br></br>
         <Slider
           value={avgHomePriceRange}
           onChange={handleHomePriceChange}
-          valueLabelDisplay="auto"
+          valueLabelDisplay="on"
           valueLabelFormat={(value) => `$${value}`}
           step = {1000}
           min={0}
           max={10000000}
         />
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={5}>
         
  <Typography gutterBottom>Rent Range</Typography>
+ <br></br><br></br>
         <Slider
           value={rentRange}
           onChange={handleRentChange}
-          valueLabelDisplay="auto"
+          valueLabelDisplay="on"
           valueLabelFormat={(value) => `$${value}`}
           step = {100}
           min={0}
@@ -84,37 +88,40 @@ const AdvancedSearchModal = ({ open, onClose, onApplyFilters }) => {
         />
         </Grid>
 
-        <Grid item xs={6}>
+        <Grid item xs={5}>
         
  <Typography gutterBottom>Tax Burden Range</Typography>
+ <br></br><br></br>
         <Slider
           value={taxBurdenRange}
           onChange={handleTaxBurdenChange}
-          valueLabelDisplay="auto"
+          valueLabelDisplay="on"
           valueLabelFormat={(value) => `${value}%`}
           step = {0.01}
           min={0}
           max={1}
         /></Grid>
-        <Grid item xs={6}>
+        <Grid item xs={5}>
         
  <Typography gutterBottom>Crime Range</Typography>
+ <br></br><br></br>
         <Slider
           value={totalCrimeRange}
           onChange={handleCrimeChange}
-          valueLabelDisplay="auto"
+          valueLabelDisplay="on"
           valueLabelFormat={(value) => `${value}`}
           step = {1}
           min={0}
           max={100000}
         /></Grid>
-        <Grid item xs={6}>
+        <Grid item xs={5}>
         
 <Typography gutterBottom>Population Range</Typography>
+<br></br><br></br>
         <Slider
           value={populationRange}
           onChange={handlePopulationChange}
-          valueLabelDisplay="auto"
+          valueLabelDisplay="on"
           valueLabelFormat={(value) => `${value}`}
           step = {10000}
           min={0}
