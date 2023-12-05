41268ebe0ceab0f903a9f984b33d898d91142b7a
Jinyeong <jinyeongpark1205@gamil.com>
2023-12-05 01:14:23 -0800

    fixed layout for citypage

--- client/src/pages/CityPage.js
+++ client/src/pages/CityPage.js
@@ -1,29 +1,24 @@
-import React from 'react'
-import LineChart from '../components/LineChart'
-import Map from '../components/Map'
-import { useParams } from 'react-router-dom'
-
-
+import React from 'react';
+import LineChart from '../components/LineChart';
+import Map from '../components/Map';
+import { useParams } from 'react-router-dom';
 
 export function CityPage() {
-  const {city, state} = useParams();
-
+  const { city, state } = useParams();
 
   return (
     <>
-      <h2 style={{ textAlign: 'center' }}>{city+", "} {state}</h2>
-      <div className="container">
-        <div className="chart-container" style={{ width: '90%', margin: '0 auto', marginBottom: '40px' }}>
+      <h2 style={{ textAlign: 'center', margin: '70px 0px' }}>{city + ', '} {state}</h2>
+      <div className="container" style={{ display: 'flex', padding: '20px', overflowX: 'hidden' }}>
+        <div className="chart-container" style={{ flex: '1', marginRight: '2%', marginBottom: '40px' }}>
           <LineChart city={city} state={state} />
         </div>
-        <div className="map-container" style={{ minWidth: '500px', textAlign:'center'}}>
+        <div className="map-container" style={{ flex: '1', paddingTop: '90px', textAlign: 'center' }}>
           <Map city={city} state={state} />
         </div>
       </div>
     </>
   );
-
 }
 
-
-export default CityPage
+export default CityPage;
